07641d2117d0017f73e8cc6fc9095e5eda5ccf7d
Run-J <jirun040403@hotmail.com>
2025-05-15 19:37:21 +0800

    541_ReversStringII.cpp

--- 541_ReverseStringII.cpp
+++ 541_ReverseStringII.cpp
@@ -21,3 +21,36 @@ public:
     }
 
 };
+
+
+// Custom Version Reverse Function
+class Solution {
+public:
+
+    string reverseStr(string s, int k) {
+
+        for (int i = 0; i < s.size(); i += (2*k) ) {
+
+            if (i + k <= s.size()) {
+                reverse(s, i, i+k-1);
+            } else {
+                reverse(s, i, s.size()-1);
+            }
+
+        }
+
+        return s;
+    }
+
+
+    void reverse(string& s, int left, int right) {
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+
+    }
+
+};
