c57124f551f62e2f8083d34ee9484697fb1e0cc6
Run Ji <146859383+Run-J@users.noreply.github.com>
2025-12-11 17:50:40 +0800

    Update 104_MaximumDepthOfBinaryTree.cpp
    Added DFS/Recursion solution

--- 104_MaximumDepthOfBinaryTree.cpp
+++ 104_MaximumDepthOfBinaryTree.cpp
@@ -10,6 +10,29 @@
  * };
  */
 
+
+// Solution 1 (Depth First Search (DFS) / Recursion (Post-order):
+// Don't try to look at the whole tree at once. Instead, think recursively.
+// The Logic: If you are a node (say, the Root), how do you know the total depth?
+//     Ask your Left Child: "What is the max depth down your path?"
+//     Ask your Right Child: "What is the max depth down your path?"
+//     Take the bigger of those two numbers, and add 1 (for yourself).
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return 1 + max(leftDepth, rightDepth);
+    }
+};
+
+
+
+
+// Solution 2 (Breadth First Search (BFS) / Iterative):
 // 用队列辅助层序遍历，记录层数
 class Solution {
 public:
